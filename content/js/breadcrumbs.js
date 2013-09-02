eac05197d9a6399196bed16cf979d8949040f874
Rob Vesse <rvesse@apache.org>
2013-09-02 19:18:01 +0000

    Add new files introduced by latest website patch (JENA-482)
    git-svn-id: https://svn.apache.org/repos/asf/jena/site/trunk@1519498 13f79535-47bb-0310-9956-ffa450edef68

---
+++ content/js/breadcrumbs.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+	var url = $(location).attr('href');
+	var prefix = 'http://localhost/';
+
+	if(url != prefix && url != prefix + 'index.html'){
+
+		var shortForm = url.replace(prefix, '').replace(/(index)?\.html/g, '').replace(/#.*/g, '');
+	
+		var crumbs = shortForm.split('/');
+
+		if(crumbs[crumbs.length-1] == ""){
+			crumbs.pop();
+		}
+
+		var link = prefix;
+	
+		var breadcrumbs = '<ol class="breadcrumb">';
+	
+		for (var index = 0; index < crumbs.length; ++index) {
+			var crumb = crumbs[index];
+			link += crumb + '/';
+
+			//Check if it is the last element of the array
+			if(crumbs.length - 1 == index){
+				//If yes then declare as active
+				breadcrumbs += '<li class="active">' + crumb.toUpperCase().replace(/_|-/g, ' ') + '</li>'; 
+			}else{
+				//if not then print a link for the category
+				breadcrumbs += '<li><a href="' + link + '">' + crumb.toUpperCase().replace(/_|-/g, ' ') + '</a></li>'; 
+			}
+		}
+		breadcrumbs += '</ol>';
+		$('#breadcrumbs').append(breadcrumbs);
+	}else{
+		$('#breadcrumbs').hide();
+	}
+});
+
+//update exporter to handle gh-pages
