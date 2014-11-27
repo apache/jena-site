fdc5a37a64562282c3bda0229ce4cf1035e12c98
Rob Vesse <rvesse@apache.org>
2014-11-27 10:22:51 +0000

    Fix JS bugs (JENA-819)
    git-svn-id: https://svn.apache.org/repos/asf/jena/site/trunk@1642075 13f79535-47bb-0310-9956-ffa450edef68

--- content/js/improve.js
+++ content/js/improve.js
@@ -1,7 +1,7 @@
 function improveThisPage( url ) {
   // Inform users how to contributing
-  alert('Thanks for contributing to the Apache Jena website, please use the username anonymous and leave the password blank if prompted for credentials';);
+  alert('Thanks for contributing to the Apache Jena website, please use the username anonymous and leave the password blank if prompted for credentials');
 
   // Redirect to the CMS
-  location.href = 'https://cms.apache.org/redirect?action=edit;uri=' + escape(url)
+  location.href = 'https://cms.apache.org/redirect?action=edit;uri=' + escape(url);
 }
