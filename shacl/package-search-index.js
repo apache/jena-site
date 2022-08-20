3ebd957926830a2e920c421382a87dcf14a92936
Andy Seaborne <andy@apache.org>
2022-08-20 11:45:57 +0100

    Javadoc 4.6.0

--- shacl/package-search-index.js
+++ shacl/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"org.apache.jena.shacl","l":"org.apache.jena.shacl"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"org.apache.jena.shacl","l":"org.apache.jena.shacl"},{"m":"org.apache.jena.shacl","l":"org.apache.jena.shacl.validation"},{"m":"org.apache.jena.shacl","l":"org.apache.jena.shacl.validation.event"}];updateSearchResults();
