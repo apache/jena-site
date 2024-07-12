fe175517797dc1d65ca516969c0284659c6bb04a
Andy Seaborne <andy@apache.org>
2024-07-12 17:58:32 +0100

    Javadoc 5.1.0

--- extras/querybuilder/module-search-index.js
+++ extras/querybuilder/module-search-index.js
@@ -1,1 +1,1 @@
-moduleSearchIndex = [];updateSearchResults();
+moduleSearchIndex = [{"l":"org.apache.jena.querybuilder"}];updateSearchResults();
---
+++ ontapi/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"org.apache.jena.ontapi"}];updateSearchResults();
